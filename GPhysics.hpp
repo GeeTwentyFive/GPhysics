e78b2d0541c2e0eb2808452e9806c45e51c3560d
Gee <alickoamarko@gmail.com>
2026-05-04 00:57:57 +0200

    WIP

--- GPhysics.hpp
+++ GPhysics.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
 class GPhysics { private: struct _internal; struct _internal* _; public: ~GPhysics();
-        GPhysics(); // TODO
+        GPhysics(unsigned int max_bodies = 65536, int tickrate = 128);
+
+        void* AddBox(float size_x, float size_y, float size_z, bool stationary); // TODO
+        void* AddSphere(float radius, bool stationary); // TODO
+        void* AddCylinder(float height, float top_radius, float bottom_radius, bool stationary); // TODO
+
+        // TODO
+
+        void Tick();
 };
